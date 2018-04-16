ea23702762017964ad8212d5f2f702fdeab2d20e
Iggy <igoririanto@rocketmail.com>
2018-04-16 07:56:42 -0500

    add start_child/1 and start_child/2

--- lib/thy_supervisor.ex
+++ lib/thy_supervisor.ex
@@ -5,10 +5,34 @@ defmodule ThySupervisor do
     GenServer.start_link(__MODULE__, [child_spec_list])
   end
 
+  def start_child(superisor, child_spec) do
+    GenServer.call(supervisor, {:start_child, child_spec})
+  end
+
   # server side
-  def init(child_spec_list) do
+  def init([child_spec_list]) do
     Process.flag(:trap_exit, true)
     state = child_spec_list |> start_children |> Enum.into(HashDict.new)
     {:ok, state}
+  end
+
+  def handle_call({:start_child, child_spec}, _from, state) do
+    case start_child(child_spec) do
+      {:ok, pid} ->
+        new_state = state |> Map.put(pid, child_spec)
+	{:reply, {:ok, pid}, new_state}
+      :error ->
+        {:reply, {:error, "error starting child!"}, state}
+    end
+  end
+
+  defp start_child({mod, fun, args}) do
+    case apply(mod, fun, args) do
+      pid when is_pid(pid) ->
+        Process.link(pid)
+	{:ok, pid}
+      _ ->
+        :error
+    end
   end
 end
